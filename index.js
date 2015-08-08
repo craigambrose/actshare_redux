f7b0ec614848b59241f93e4ae39e56553733a604
Craig Ambrose <craig@craigambrose.com>
2015-08-08 22:02:52 +1200

    added bootstrap

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import App from './containers/App';
-import './assets/stylesheets/main.scss';
+//import './assets/stylesheets/_main.scss';
+
 
 React.render(
 <App />,
   document.getElementById('root')
 );
+
+require("bootstrap-sass!./assets/stylesheets/bootstrap-sass.config.js");
--- webpack.config.js
+++ webpack.config.js
@@ -27,14 +27,16 @@ module.exports = {
         loaders: ['react-hot', 'babel'],
         exclude: /node_modules/
       },
-      {
-        test: /\.scss$/,
-        loader: 'style!css!sass'
-      },
-      {
-        test: /\.sass$/,
-        loader: "style!css!sass?indentedSyntax"
-      }
+      { test: /\.scss$/, loader: 'style!css!sass' },
+      { test: /\.sass$/, loader: "style!css!sass?indentedSyntax" },
+      // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
+      // loads bootstrap's css.
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,  loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }
+
     ]
   }
 };
