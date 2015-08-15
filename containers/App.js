44d26ec33251ee1566d2febe0f3bbb23fda76fae
Craig Ambrose <craig@craigambrose.com>
2015-08-16 11:16:22 +1200

    enabled redux dev tools

--- containers/App.js
+++ containers/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import ActShareApp from './ActShareApp';
-import { createStore, combineReducers } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
 import * as reducers from '../reducers';
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+import thunk from 'redux-thunk';
+
+const finalCreateStore = compose(
+  applyMiddleware(thunk),
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+  createStore
+);
 
 const reducer = combineReducers(reducers);
-const store = createStore(reducer);
+const store = finalCreateStore(reducer);
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        {() => <ActShareApp />}
-      </Provider>
+      <div>
+        <Provider store={store}>
+          {() => <ActShareApp />}
+        </Provider>
+        {/*<DebugPanel top right bottom>
+          <DevTools store={store} monitor={LogMonitor} />
+        </DebugPanel>*/}
+      </div>
     );
   }
 }
